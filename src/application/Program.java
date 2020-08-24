511ba99ea5e88883d1746771ac93caf547952819
Oswaldo Cristovam <oswaldo.jose.c@hotmail.com>
2020-08-24 15:00:00 -0300

    Recuperando dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import db.DB;
 
@@ -9,7 +11,31 @@ public class Program {
 	public static void main(String[] args) {
 
 		Connection conn = DB.getConnection();
-		DB.closeConnection();
+		StringBuilder sb = new StringBuilder();
+		sb.append("Select * ");
+		sb.append("  from department ");
+		
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.createStatement();
+			rs = st.executeQuery(sb.toString());
+			
+			while (rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+		
+		
+		
 	}
 
 }
